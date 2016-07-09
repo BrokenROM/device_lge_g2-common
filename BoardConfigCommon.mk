CyanogenMod/android_device_lge_g2-common

g2: Support for native tap-to-wake
Change-Id: I80998dbe631468be269c6f7d8e2284111b7d245d

BoardConfigCommon.mk
@@ -64,6 +64,7 @@ COMMON_GLOBAL_CFLAGS += \
 
 # CMHW
 BOARD_HARDWARE_CLASS := device/lge/g2-common/cmhw/
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/tap_to_wake"
 
 # Display
 HAVE_ADRENO_SOURCE := false

BrokenROM/device_lge_g2-common

BoardConfigCommon.mk
@@ -76,6 +76,7 @@ COMMON_GLOBAL_CFLAGS += \
 
 # CMHW
 BOARD_HARDWARE_CLASS := device/lge/g2-common/cmhw/
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/touch_gesture"
 
 # Display
 HAVE_ADRENO_SOURCE := false